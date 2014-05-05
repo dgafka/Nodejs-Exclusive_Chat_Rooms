5f3bab87053d5e3637e82ea7cb9be0045a4dfef7
dgafka <dariss6666@gmail.com>
2014-05-05 19:55:49 +0200

    Downgraded express. Finished message sending via websockets to another chat clients

--- public/js/application.js
+++ public/js/application.js
@@ -3,16 +3,23 @@ $(document).ready(function() {
     // Create SocketIO instance
     var server = io.connect('http://localhost:3000');
 
-    //Append messages to an chat
+    //Recive message and appends message to chat
     server.on('messages', function(data) {
-        $('.text-chat').append(data.results);
+        //escape js and html
+        $('.text-chat').append($('<div></div>').text(data.results));
     });
 
     //Send a message to the server
     $('#messageForm').on("submit", function(event){
-        var content = $('#contentMessage').val();
-        server.emit('messages', {'results': content});
+        //Manipulate data
+        var content = $(this).children('#contentMessage').val();
+        $(this).parent().parent().children('.text-chat').append(content + "<br/>");
+        $(this).children('#contentMessage').val('')
 
+        //Emits a message to an server.
+        server.emit('messages', {'results': content + "<br/>"});
+
+        //Prevent submiting the form
         event.preventDefault();
     });
 
--- server.js
+++ server.js
@@ -8,32 +8,41 @@ var path = require('path');
 var less = require('less-middleware');
 
 //Configuration
-    //client libaries
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.use(express.static(path.join(__dirname, 'bower_components/jquery/dist')));
-    app.use(express.static(path.join(__dirname, 'bower_components/bootstrap/dist')));
+app.configure(function () {
+
 
     //view folder
     app.set('views', __dirname + '/views');
     //view type
     app.set('view engine', 'ejs');
-    //Stuff needed for views
-    app.use(require('body-parser')());
-
-    //Change secret session, before running the app
-    //Stuff need for sessions
-    app.use(require('cookie-session')({
-        keys: ['secret1', 'secret2']
-    }));
+    //Stuff needed for views && sessions
+    app.use(express.json());
+    app.use(express.urlencoded());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser('bardzo tajne aqq'));
+    app.use(express.session());
+    app.use(app.router);
 
     //Less compiler
     app.use(less({
         src: path.join(__dirname, 'less'),
         dest: path.join(__dirname, 'public/css'),
-        prefix: '/css',
         compress: true
     }));
 
+    //client libaries
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, 'bower_components/jquery/dist')));
+    app.use(express.static(path.join(__dirname, 'bower_components/bootstrap/dist')));
+
+});
+
+app.configure('development', function () {
+    app.use(express.logger('dev'));
+    app.use(express.errorHandler());
+});
+
+//routes
 app.get('/', routes.index);
 
 //server start listing
@@ -44,7 +53,7 @@ io.sockets.on('connection', function(client){
     console.log('Client connected...');
 
     //Emits a welcome message to client
-    client.emit('messages', { results: 'Welcome to chat.' });
+    client.emit('messages', { results: 'Connected to chat...' });
 
     client.on('messages', function(data) {
         client.broadcast.emit("messages", data);
